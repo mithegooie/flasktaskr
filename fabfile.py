4ec844cfe8e7d80d4cecf07b1e38e1ada85953d4
David Miller <mithegooie@gmail.com>
2014-09-27 20:36:44 -0700

    'fabric not working'

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import local
+
+
+def test():
+    local("nosetests -v")
+
+def commit():
+    message = raw_input("Enter a git commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
+
+def push():
+    local("git push -u origin master")
+
+def prepare():
+    test()
+    commit()
+    push()
